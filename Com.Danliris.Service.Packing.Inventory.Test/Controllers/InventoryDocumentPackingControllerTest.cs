f2ed5ad41efe81673f2054e8bf23936b2e56a896
sama08 <usamah_alan@hotmail.com>
2019-12-16 11:57:43 +0700

    sa

---
+++ Com.Danliris.Service.Packing.Inventory.Test/Controllers/InventoryDocumentPackingControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.InventoryDocumentPacking;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Com.Danliris.Service.Packing.Inventory.WebApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers
+{
+    public class InventoryDocumentPackingControllerTest
+    {
+
+        [Fact]
+        public async Task Should_Succes_OnPost()
+        {
+            var serviceMock = new Mock<IInventoryDocumentPackingService>();
+            serviceMock.Setup(s => s.Create(It.IsAny<CreateInventoryDocumentPackingViewModel>())).Returns(Task.FromResult(0));
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var controller = new InventoryDocumentPackingController(serviceMock.Object, identityProviderMock.Object);
+            
+            var user = new Mock<ClaimsPrincipal>();
+            var claims = new Claim[]
+            {
+                new Claim("username", "unittestusername")
+            };
+
+            user.Setup(u => u.Claims).Returns(claims);
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = user.Object, 
+                }
+            };
+            controller.ControllerContext = controllerContext;
+            controller.ControllerContext.HttpContext.Request.Headers["Authorization"] = "Bearer unittesttoken";
+            controller.ControllerContext.HttpContext.Request.Path = new PathString("/v1/unit-test");
+
+            var response = await controller.Post(new CreateInventoryDocumentPackingViewModel());
+
+            var statusCode = (HttpStatusCode) response.GetType().GetProperty("StatusCode").GetValue(response, null);
+
+            Assert.Equal(HttpStatusCode.Created, statusCode);
+        }
+    }
+}
---
+++ Com.Danliris.Service.Packing.Inventory.Test/Controllers/ProductControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.Product;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Com.Danliris.Service.Packing.Inventory.WebApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers
+{
+    
+    public class ProductControllerTest
+    {
+        [Fact]
+        public async Task Should_Succes_OnPost()
+        {
+            var serviceMock = new Mock<IProductService>();
+            serviceMock.Setup(service => service.CreateProductPackAndSKU(It.IsAny<CreateProductPackAndSKUViewModel>())).ReturnsAsync(new ProductPackingBarcodeInfo("", 1, 2, "", "", 1));
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var controller = new ProductController(serviceMock.Object, identityProviderMock.Object);
+
+            var user = new Mock<ClaimsPrincipal>();
+            var claims = new Claim[]
+            {
+                new Claim("username", "unittestusername")
+            };
+            user.Setup(u => u.Claims).Returns(claims);
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = user.Object
+                }
+            };
+            controller.ControllerContext.HttpContext.Request.Headers["Authorization"] = "Bearer Token";
+            controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = "7";
+            controller.ControllerContext.HttpContext.Request.Path = new PathString("/v1/unit-test");
+
+            var response = controller.Post(new CreateProductPackAndSKUViewModel());
+
+            Assert.NotNull(response);
+        }
+        
+
+    }
+}
